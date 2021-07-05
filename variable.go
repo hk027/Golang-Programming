8ca0601bea9afa28c21e421ec758dc9c81414c83
Kalonji Kalala <hkalo081@uottawa.ca>
2021-07-05 13:13:26 -0400

    Create variable.go

---
+++ variable.go
@@ -0,0 +1,14 @@
+package main 
+import ("fmt")
+
+func main(){
+	var i int = 10 
+	var s string = "Canada"
+	var k float64 = 27.05
+	var g string = "The planet hearth is not flat"
+	fmt.Println(i)
+	fmt.Println(s)
+	fmt.Println(g)
+	fmt.Println("the total is", k)
+
+}
