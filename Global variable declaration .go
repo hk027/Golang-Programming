9e68b0b3cd62063ec87540753c0e2ad66855c60c
Kalonji Kalala <hkalo081@uottawa.ca>
2021-07-05 13:55:22 -0400

    Create Global variable declaration .go

---
+++ Global variable declaration .go
@@ -0,0 +1,21 @@
+package main 
+import "fmt"
+
+//Global variable declaration 
+
+var (
+	m int 
+	n int 
+	k float64)
+
+	func main() {
+		var x int = 10 // Integer Data type 
+		var y int  = 27// Integer Data type
+		var m float64 = 28.948
+		fmt.Println(x)
+		fmt.Println(y)
+		fmt.Println(m)
+
+		var a,b,c = 10.34, 11.34, 13.99
+		fmt.Println("Here are the three variable:",a,"\t--", b,"\t--",c)
+	}
